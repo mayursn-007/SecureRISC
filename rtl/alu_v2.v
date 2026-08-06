3903fb79d5cd2d2896d5ab86b75f32cd71001d44
Mayur S Nagoji <24ug1byec157@bmsit.in>
2026-08-06 19:53:30 +0530

    Started ALU v2 redesign

---
+++ rtl/alu_v2.v
@@ -0,0 +1,7 @@
+//===========================================================
+// Project : SecureRISC SR32
+// Module  : ALU
+// Version : v0.2
+// Status  : Under Development
+// Author  : Mayur S Nagoji
+//===========================================================
