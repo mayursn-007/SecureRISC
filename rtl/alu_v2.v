da97066198154e8e0b72df2b96dffdc963423736
Mayur S Nagoji <24ug1byec157@bmsit.in>
2026-08-08 20:41:30 +0530

    Update alu_v2.v

--- rtl/alu_v2.v
+++ rtl/alu_v2.v
@@ -22,10 +22,13 @@ module alu_v2
 
     output reg [DATA_WIDTH-1:0] result,
 
-    output reg carry_flag,
-    output reg overflow_flag,
+output reg carry_flag,
+output reg overflow_flag,
+output zero_flag,
+output negative_flag
+);
     assign zero_flag = (result == 0);
-    assign negative_flag = result[DATA_WIDTH-1];
+assign negative_flag = result[DATA_WIDTH-1];
     //===========================================================
 // Combinational Logic
 //===========================================================
@@ -133,8 +136,5 @@ end
     endcase
 
 end
-
-end
-);
 
 endmodule
